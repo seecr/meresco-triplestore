d32804966cc8a69022b3b2cd70da03282f060dfa
JJ <JJ@7f82bda0-831b-0410-895c-c142f06d1e64>
2011-02-02 15:45:46 +0000

    JJ/JPM: OwlimTripleStore now supports executeQuery with sparql query
    git-svn-id: svn+ssh://svn.seecr.nl/svn/owlimhttpserver/trunk@15606 7f82bda0-831b-0410-895c-c142f06d1e64

--- src/java/org/meresco/owlimhttpserver/OwlimTripleStore.java
+++ src/java/org/meresco/owlimhttpserver/OwlimTripleStore.java
@@ -3,18 +3,33 @@ package org.meresco.owlimhttpserver;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
+import java.io.OutputStream;
+import java.io.ByteArrayOutputStream;
 
 import com.ontotext.trree.owlim_ext.Repository;
 import com.ontotext.trree.owlim_ext.SailImpl;
+
+import org.openrdf.query.resultio.TupleQueryResultFormat;
+import org.openrdf.query.resultio.QueryResultIO;
+
+import org.openrdf.query.MalformedQueryException;
+import org.openrdf.query.QueryEvaluationException;
+import org.openrdf.query.QueryLanguage;
+import org.openrdf.query.TupleQuery;
+import org.openrdf.query.TupleQueryResult;
+import org.openrdf.query.TupleQueryResultHandlerException;
+
 import org.openrdf.model.impl.URIImpl;
 import org.openrdf.model.Resource;
 import org.openrdf.model.Statement;
 import org.openrdf.model.URI;
 import org.openrdf.model.Value;
+
 import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryException;
 import org.openrdf.repository.RepositoryResult;
 import org.openrdf.repository.sail.SailRepository;
+
 import org.openrdf.rio.RDFFormat;
 import org.openrdf.rio.RDFParseException;
 
@@ -57,10 +72,62 @@ public class OwlimTripleStore implements TripleStore {
     }
 
     public void delete(String identifier) {
+        URI context = new URIImpl(identifier);
+        RepositoryConnection conn = null;
+        try {
+            conn = repository.getConnection();
+            conn.setAutoCommit(false);
+            conn.clear(context);
+        } catch (RepositoryException e) {
+            throw new RuntimeException(e);
+        } finally {
+            close(conn);
+        }
     }
 
+    public long size() {
+        RepositoryConnection conn = null;
+        try {
+            conn = repository.getConnection();
+            return conn.size();
+        } catch (RepositoryException e) {
+            throw new RuntimeException(e);
+        } finally {
+            close(conn);
+        }
+    }
+
+
     public String executeQuery(String sparQL) {
-        throw new UnsupportedOperationException("!");
+        RepositoryConnection conn = null;
+        TupleQuery tupleQuery = null;
+        TupleQueryResult tupleQueryResult = null;
+        String result = null;
+        try {
+
+            try {
+                conn = repository.getConnection();
+                tupleQuery = conn.prepareTupleQuery(QueryLanguage.SPARQL, sparQL);
+                tupleQueryResult = tupleQuery.evaluate();
+                ByteArrayOutputStream o = new ByteArrayOutputStream();
+                QueryResultIO.write(tupleQueryResult, TupleQueryResultFormat.SPARQL, o);
+                result = o.toString("UTF-8");
+                tupleQueryResult.close();
+            } catch (QueryEvaluationException e) {
+                e.printStackTrace();
+            } catch (MalformedQueryException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (TupleQueryResultHandlerException e) {
+                throw new RuntimeException(e);
+            }
+            return result;
+        } catch (RepositoryException e) {
+            throw new RuntimeException(e);
+        } finally {
+            close(conn);
+        }
     }
 
     public RepositoryResult<Statement> getStatements(Resource subject, URI predicate, Value object) {
--- src/test/org/meresco/owlimhttpserver/OwlimTripleStoreTest.java
+++ src/test/org/meresco/owlimhttpserver/OwlimTripleStoreTest.java
@@ -45,22 +45,37 @@ public class OwlimTripleStoreTest {
         "             xmlns:exterms='http://www.example.org/terms/'>" + 
         "  <rdf:Description rdf:about='http://www.example.org/index.html'>" +
         "      <exterms:creation-date>August 16, 1999</exterms:creation-date>" +
+        "      <rdf:value>A.M. Özman Yürekli</rdf:value>" + 
         "  </rdf:Description>" +
         "</rdf:RDF>";
 
     @Test
     public void testAddRDFGetStatements() throws Exception {
-        RepositoryResult<Statement> statements = ts.getStatements(null, null, null);
-        int startingPoint = statements.asList().size();
+        long startingPoint = ts.size();
         ts.addRDF("uri:id0", rdf, RDFFormat.RDFXML);
-        statements = ts.getStatements(null, null, null);
-        assertEquals(startingPoint + 1, statements.asList().size());
+        RepositoryResult<Statement> statements = ts.getStatements(null, null, null);
+        assertEquals(startingPoint + 2, statements.asList().size());
         List<Statement> statementList = ts.getStatements(new URIImpl("http://www.example.org/index.html"), null, null).asList();
-        assertEquals(1, statementList.size());
+        assertEquals(2, statementList.size());
         assertEquals(new LiteralImpl("August 16, 1999"), statementList.get(0).getObject());
-        
-
-
+        assertEquals(new LiteralImpl("A.M. Özman Yürekli"), statementList.get(1).getObject());
     }
 
+    @Test
+    public void testDelete() throws Exception {
+        ts.addRDF("uri:id0", rdf, RDFFormat.RDFXML);
+        long startingPoint = ts.size();
+        ts.delete("uri:id0");
+        assertEquals(startingPoint - 2, ts.size());
+    }
+
+    @Test
+    public void testSparql() throws Exception {
+        String answer = null;
+
+        ts.addRDF("uri:id0", rdf, RDFFormat.RDFXML);
+        answer = ts.executeQuery("SELECT ?x ?y ?z WHERE {?x ?y ?z}");
+        assertTrue(answer.indexOf("<literal>A.M. Özman Yürekli</literal>") > -1);
+        assertTrue(answer.endsWith("</sparql>\n"));
+    }
 }
