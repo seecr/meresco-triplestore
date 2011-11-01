78a71cdcd7ce2e6e6e92edba28e2d2b807f2b4e1
Seecr <development@seecr.nl>
2011-11-01 10:20:16 +0100

    HM/JJ: applied license

--- test/alltests.sh
+++ test/alltests.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 ## begin license ##
 # 
-# "Meresco RDF OWLIM" provides classes for interacting with the OWLIM
-# triplestore in Meresco 
+# A Python binding using HTTP to communicate with an Owlim HTTP Server. 
 # 
 # Copyright (C) 2011 Seecr (Seek You Too B.V.) http://seecr.nl
-# Copyright (C) 2011 Seek You Too (CQ2) http://www.cq2.nl
-# Copyright (C) 2011 Stichting Kennisnet http://www.kennisnet.nl
 # 
-# This file is part of "Meresco RDF OWLIM"
+# This file is part of "OwlimHttpClient"
 # 
-# "Meresco RDF OWLIM" is free software; you can redistribute it and/or modify
+# "OwlimHttpClient" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 # 
-# "Meresco RDF OWLIM" is distributed in the hope that it will be useful,
+# "OwlimHttpClient" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
-# along with "Meresco RDF OWLIM"; if not, write to the Free Software
+# along with "OwlimHttpClient"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 # 
 ## end license ##
