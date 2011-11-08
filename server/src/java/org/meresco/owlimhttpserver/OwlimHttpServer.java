a4708911b89ed90f9991514b61b0b55406f4c072
Johan Jonkers <johan@cq2.nl>
2011-11-08 16:35:02 +0100

    JJ: updated license

--- server/src/java/org/meresco/owlimhttpserver/OwlimHttpServer.java
+++ server/src/java/org/meresco/owlimhttpserver/OwlimHttpServer.java
@@ -1,24 +1,26 @@
 /* begin license *
  * 
- * "OwlimHttpServer" provides a simple HTTP interface to an OWLim triplestore. 
+ * The Meresco Owlim package consists out of a HTTP server written in Java that
+ * provides access to an Owlim Triple store, as well as python bindings to
+ * communicate as a client with the server. 
  * 
  * Copyright (C) 2011 Seecr (Seek You Too B.V.) http://seecr.nl
  * Copyright (C) 2011 Seek You Too B.V. (CQ2) http://www.cq2.nl
  * 
- * This file is part of "OwlimHttpServer"
+ * This file is part of "Meresco Owlim"
  * 
- * "OwlimHttpServer" is free software; you can redistribute it and/or modify
+ * "Meresco Owlim" is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  * 
- * "OwlimHttpServer" is distributed in the hope that it will be useful,
+ * "Meresco Owlim" is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with "OwlimHttpServer"; if not, write to the Free Software
+ * along with "Meresco Owlim"; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  * 
  * end license */
