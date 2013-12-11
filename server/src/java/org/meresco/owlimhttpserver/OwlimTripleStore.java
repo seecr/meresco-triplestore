f94d544bf1e3d7574f3e705fe8670d5fe638c52e
Hendrik Moree <hendrik@seecr.nl>
2013-12-11 11:40:55 +0100

    JJ/HM: Set syncdelay

--- server/src/java/org/meresco/owlimhttpserver/OwlimTripleStore.java
+++ server/src/java/org/meresco/owlimhttpserver/OwlimTripleStore.java
@@ -82,8 +82,9 @@ public class OwlimTripleStore implements TripleStore {
 
     public OwlimTripleStore(File directory, String storageName) {
     	this.directory = directory;
-        MemoryStore owlimSail = new MemoryStore(directory);
-        repository = new SailRepository(owlimSail);
+        MemoryStore sail = new MemoryStore(directory);
+        sail.setSyncDelay(60000L);
+        repository = new SailRepository(sail);
         // owlimSail.setParameter(Repository.PARAM_STORAGE_FOLDER, storageName);
         // owlimSail.setParameter("ruleset", "empty");
         // repository.setDataDir(directory);
