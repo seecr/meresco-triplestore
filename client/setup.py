db169812914ac7ac425613076f6a9c52148c8802
Seecr Development Team <development@seecr.nl>
2014-01-09 09:28:52 +0100

    HM: Updated setup.py

--- client/setup.py
+++ client/setup.py
@@ -1,27 +1,27 @@
 ## begin license ##
-# 
-# The Meresco Owlim package consists out of a HTTP server written in Java that
-# provides access to an Owlim Triple store, as well as python bindings to
-# communicate as a client with the server. 
-# 
-# Copyright (C) 2011-2013 Seecr (Seek You Too B.V.) http://seecr.nl
-# 
-# This file is part of "Meresco Owlim"
-# 
-# "Meresco Owlim" is free software; you can redistribute it and/or modify
+#
+# The Meresco Triplestore package consists out of a HTTP server written in Java that
+# provides access to an Triplestore with a Sesame Interface, as well as python bindings to
+# communicate as a client with the server.
+#
+# Copyright (C) 2011-2014 Seecr (Seek You Too B.V.) http://seecr.nl
+#
+# This file is part of "Meresco Triplestore"
+#
+# "Meresco Triplestore" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
-# "Meresco Owlim" is distributed in the hope that it will be useful,
+#
+# "Meresco Triplestore" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
-# along with "Meresco Owlim"; if not, write to the Free Software
+# along with "Meresco Triplestore"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
 from distutils.core import setup
@@ -36,8 +36,8 @@ setup(
     url='http://www.seecr.nl',
     author='Seecr (Seek You Too B.V.)',
     author_email='info@seecr.nl',
-    description='A Python binding to communicate with an Owlim HTTP Server.',
-    long_description='A Python binding using HTTP to communicate with an Owlim HTTP Server.',
+    description='A Python binding to communicate with an Sesame Interace HTTP Server.',
+    long_description='A Python binding using HTTP to communicate with an Sesame Interface HTTP Server.',
     license='GNU Public License',
     platforms='all',
 )
