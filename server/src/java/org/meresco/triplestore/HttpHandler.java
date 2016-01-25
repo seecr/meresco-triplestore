4014195c8bdd026251170a166fcf419175030597
Thijs Janssen <thijs@seecr.nl>
2016-01-25 14:53:06 +0100

    HM/TJ: use getInputStream instead of getReader

--- server/src/java/org/meresco/triplestore/HttpHandler.java
+++ server/src/java/org/meresco/triplestore/HttpHandler.java
@@ -1,26 +1,26 @@
 /* begin license *
  *
- * The Meresco Owlim package consists out of a HTTP server written in Java that
- * provides access to an Owlim Triple store, as well as python bindings to
+ * The Meresco Triplestore package consists out of a HTTP server written in Java that
+ * provides access to an Triplestore with a Sesame Interface, as well as python bindings to
  * communicate as a client with the server.
  *
- * Copyright (C) 2011-2014 Seecr (Seek You Too B.V.) http://seecr.nl
+ * Copyright (C) 2011-2014, 2016 Seecr (Seek You Too B.V.) http://seecr.nl
  * Copyright (C) 2011 Seek You Too B.V. (CQ2) http://www.cq2.nl
  *
- * This file is part of "Meresco Owlim"
+ * This file is part of "Meresco Triplestore"
  *
- * "Meresco Owlim" is free software; you can redistribute it and/or modify
+ * "Meresco Triplestore" is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * "Meresco Owlim" is distributed in the hope that it will be useful,
+ * "Meresco Triplestore" is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with "Meresco Owlim"; if not, write to the Free Software
+ * along with "Meresco Triplestore"; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  * end license */
@@ -201,7 +201,7 @@ public class HttpHandler extends AbstractHandler {
                 parameters.put(key, values);
         }
         if (request.getMethod().equals("POST")) {
-            parameters.putAll(Utils.parseQS(Utils.read(request.getReader())));
+            parameters.putAll(Utils.parseQS(Utils.read(request.getInputStream())));
         }
         return parameters;
     }
